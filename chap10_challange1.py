dce15b8696ba5482e56fe53cbc4f3ddad6e3ebed
germanopasen <168157840+germanopasen@users.noreply.github.com>
2024-04-26 22:25:27 +0300

    Create chap10_challange1.py

---
+++ chap10_challange1.py
@@ -0,0 +1,8 @@
+C:\Users\11\AppData\Local\Temp\6c975bbc-f5a4-4b06-ae20-9f49944cd333_self_taught_programmer_althoff.zip.333\Self_Taught_Programmer_Althoff\Глава 10\chap10_challenge1.py:9: SyntaxWarning: invalid escape sequence '\ '
+  stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+C:\Users\11\AppData\Local\Temp\6c975bbc-f5a4-4b06-ae20-9f49944cd333_self_taught_programmer_althoff.zip.333\Self_Taught_Programmer_Althoff\Глава 10\chap10_challenge1.py:9: SyntaxWarning: invalid escape sequence '\ '
+  stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+Добро пожаловать на казнь
+
+
+Введите букву:
